b264502e26f20a273c431f8613f67ec63521e748
Guanzhuo Qiao <gqiao@stevens.edu>
2020-01-18 11:40:00 +0800

    compute labels

--- label_generator.py
+++ label_generator.py
@@ -8,17 +8,18 @@ def rolling_sr(stock_time_series,period):
     res = annual_r/vol
     return res
 
-
-
-def get_stock_performance(dt: pd.DataFrame,func: "function",layer_num: int,*args) -> pd.DataFrame:
+def get_stock_performance(dt: pd.DataFrame,func: "function",layer_num: int,*args,**kwargs) -> pd.DataFrame:
     """
     dt: 2d pd.DataFrame, rows: time; columns: stock symbols
-    func: the function of computing the performance. like sharpe ratio
+    func: the function of computing the performance. like sharpe ratio. implemented on one stock time series
     layer_num: the number of layers that the stocks can be classified
+    *args & **kwargs: parameters given to the func
     return: 2d pd.DataFrame, labels of the each stocks at each time
     """
-    dt.apply(func,axis=0,args=5,raw=True)
-
+    ind_table = dt.apply(func,axis=0,*args,**kwargs)
+    ind_table = ind_table.rank(axis=1,pct=True,ascending=True)
+    ind_table = ind_table.apply(lambda x: x//(1/layer_num)+1, axis=1)
+    return ind_table
 
 
 
